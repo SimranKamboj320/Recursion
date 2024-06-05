ff37732749bf15b82d0ec90ddd965f7faf938c14
SimranKamboj320 <simrankamboj320@gmail.com>
2024-06-06 00:45:40 +0530

    First Occurance
    First Occurance of element at which index

---
+++ FirstOccurance.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Recursion {
+    public static int FirstOccurance(int arr[], int key, int i) {
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return FirstOccurance(arr, key, i+1);
+    }
+    public static void main(String args[]) {
+        int arr[] ={8,3,6,9,5,10,2,5,3};
+        System.out.println(FirstOccurance(arr, 5, 0));
+    }
+}
