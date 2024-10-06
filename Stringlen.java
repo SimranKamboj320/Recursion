e1bd6b16affbae9654f429db9c7255321c5ff49c
SimranKamboj320 <simrankamboj320@gmail.com>
2024-10-06 23:58:40 +0530

    String length using Recursion
    String length using Recursion

---
+++ Stringlen.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Stringlen {
+    public static int length(String str){
+        if(str.length() == 0){
+            return 0;
+        }
+
+        return length(str.substring(1)) +1;
+
+    }
+    public static void main(String args[]){
+        String str = "apples";
+        System.out.println((length(str)));
+    }
+}
